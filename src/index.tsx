5879c7ee030b2cdce6871796ebfe999162debd63
Moreal <dev.moreal@gmail.com>
2021-08-03 00:20:44 +0900

    feat: initialize project

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, Text } from 'ink';
+
+const Command = () => {
+    return (<Text color="blueBright">Nine Chronicles on Command Line</Text>);
+};
+
+render(<Command />);
